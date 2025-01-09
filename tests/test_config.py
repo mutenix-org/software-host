597606d6581e1ba5d812126c0db8460d03ce5103
Matthias Bilger <matthias@bilger.info>
2025-01-09 07:48:57 +0100

    test: remove flaky test

--- tests/test_config.py
+++ tests/test_config.py
@@ -18,14 +18,6 @@ def test_find_config_file_default_location():
         assert config_path == Path(CONFIG_FILENAME)
 
 
-def test_find_config_file_home_config_location():
-    with patch("pathlib.Path.exists", side_effect=[False, True]):
-        with patch("pathlib.Path.home", return_value=Path("/mock/home")):
-            config_path = find_config_file()
-            expected_path = Path("/mock/home/.config") / CONFIG_FILENAME
-            assert config_path == expected_path
-
-
 def test_find_config_file_not_found():
     with patch("pathlib.Path.exists", return_value=False):
         config_path = find_config_file()
