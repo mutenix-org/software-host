132e701d1ded88dbb7bf2011858b115274559c41
Matthias Bilger <matthias@bilger.info>
2025-02-08 11:47:52 +0100

    refactor, test: device messages

---
+++ tests/test_device_update_log_messages.py
@@ -0,0 +1,44 @@
+from mutenix.updates.device_messages import LogMessage
+
+
+def test_log_message_valid_debug():
+    data = b"LDThis is a debug message\x00"
+    log_message = LogMessage(data)
+    assert log_message.is_valid
+    assert log_message.level == "debug"
+    assert log_message.message == "This is a debug message"
+    assert str(log_message) == "debug: This is a debug message"
+
+
+def test_log_message_valid_error():
+    data = b"LEThis is an error message\x00"
+    log_message = LogMessage(data)
+    assert log_message.is_valid
+    assert log_message.level == "error"
+    assert log_message.message == "This is an error message"
+    assert str(log_message) == "error: This is an error message"
+
+
+def test_log_message_invalid_identifier():
+    data = b"XXInvalid identifier\x00"
+    log_message = LogMessage(data)
+    assert not log_message.is_valid
+    assert log_message.message == ""
+    assert str(log_message) == "Invalid Request"
+
+
+def test_log_message_no_null_terminator():
+    data = b"LEThis is an error message"
+    log_message = LogMessage(data)
+    assert log_message.is_valid
+    assert log_message.level == "error"
+    assert log_message.message == "This is an error message"
+    assert str(log_message) == "error: This is an error message"
+
+
+def test_log_message_empty_data():
+    data = b""
+    log_message = LogMessage(data)
+    assert not log_message.is_valid
+    assert log_message.message == ""
+    assert str(log_message) == "Invalid Request"
